09ea02e0db0f9c603f4f1ba38dccdd45193a3761
tispar <103929258+tispar@users.noreply.github.com>
2022-05-03 15:03:19 +0200

    Guess folder complete

---
+++ 01_GuessNumbers/guess.py
@@ -0,0 +1,20 @@
+# This is a number guessing game
+import random 
+
+guessesTaken = 0
+
+#Getting the name of the user
+
+
+#Setting the random number for the user to guess
+
+
+#Asks for up to 6 times for a number input of the user 
+
+    #Feedback
+
+    #Exit loop if user guessed the right number
+
+#Outputs for correct guess
+
+#Output for no. of guesses exceeded
