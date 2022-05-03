2233cc219e04cad950c4f88497e7346853b7eb3e
tispar <103929258+tispar@users.noreply.github.com>
2022-05-03 14:46:48 +0200

    Updated guess.py and created task file for guessing game

--- 01_GuessNumbers/guess.py
+++ 01_GuessNumbers/guess.py
@@ -1,25 +1,30 @@
-#  Dies ist ein Zahlenratespiel.
-import random
+# This is a number guessing game
+import random 
 
 guessesTaken = 0
 
+#Getting the name of the user
 print('Hello! What is your name?')
 myName = input()
 
+#Setting the random number for the user to guess
 number = random.randint(1,20)
 print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
+#Asks for up to 6 times for a number input of the user 
 for guessesTaken in range(6):
     print('Take a guess.')
     guess = input()
     guess = int(guess)
 
+    #Feedback
     if guess < number:
         print('Your guess is too low.')
 
     if guess > number:
         print('Your guess is too high.')
 
+    #Exit loop if user guessed the right number
     if guess == number:
         break
 
